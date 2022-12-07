6f66c65bd31875f9cc34a98eed6a50699fd9e0e7
MartinKratochvilProgramy <martvil96@gmail.com>
2022-12-07 10:15:19 +0100

    added tsconfig

--- server.ts
+++ server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express = require('express');
 import { TickerDataInterface } from './types/TickerDataInterface';
 import { getDCAValues, getHistoricalData, getRelativeChange } from './functions';
 
